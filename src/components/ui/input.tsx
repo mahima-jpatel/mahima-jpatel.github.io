1847e440a107dccc8389934fa492c035200575c7
mahima-jpatel <mahimapateladm@gmail.com>
2025-09-05 09:59:33 -0400

    Create input.tsx

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,4 @@
+import * as React from 'react'
+export function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
+  return <input {...props} className={`h-10 w-full rounded-xl border border-slate-300 bg-white px-3 text-sm outline-none focus:ring-2 focus:ring-slate-300 ${props.className || ''}`} />
+}
